4bd7f8a24042fa85968ed1cc7add60bdd7b2a083
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-01-23 18:55:30 +0700

    change box to card, use react-slick

--- src/components/Felicitation.jsx
+++ src/components/Felicitation.jsx
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from "react"
 import axios from "axios"
 import { Box } from "@mui/material"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Felicitation = () => {
 
@@ -11,33 +14,38 @@ const Felicitation = () => {
     }, [])
 
     const getConfirmationData = async () => {
-        const {data} = await axios.get('https://mocki.io/v1/93cf1e97-9c11-4c6b-8146-721e0688aac7')
+        const {data} = await axios.get('https://mocki.io/v1/311f608f-6aba-40a3-bd25-b8949fab99ec')
         setConfirmation(data)
     }
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        vertical: true,
+        verticalSwiping: true,
+        beforeChange: function(currentSlide, nextSlide) {
+          console.log("before change", currentSlide, nextSlide);
+        },
+        afterChange: function(currentSlide) {
+          console.log("after change", currentSlide);
+        }
+    }
+
     return(
-        <div className="mx-auto">
+        <div className="mx-auto felicitation">
             <div className="text-center">
                 <h1 className="uppercase md:text-4xl sm:text-2xl text-xl font-bold text-[#00df9a]" id="confirmation-title">Felicitation</h1> <br />
             </div>
-            <div className="mx-auto text-white ">
+            <Slider {...settings}>
                 {confirmations.map(confirmation => (
-                    <div className="mx-auto px-5 text-white mt-10">
-                        <Box className="text-center" sx={
-                            {
-                                backgroundColor: "#00df9a",
-                                border: '3px dashed yellow',
-                                '&:hover': {
-                                    backgroundColor: "primary.light",
-                                }
-                            }
-                        }>
-                            <br/> <p className="md:text-xl sm:text-xl text-xl">{confirmation.greetings}</p> <br/>
-                            <p className="md:text4xl sm:text-xl text-xl font-bold">-- {confirmation.name} --</p> <br/>
-                        </Box>
+                    <div className="card text-white text-center">
+                        <br/> <p className="md:text-xl sm:text-xl text-xl">{confirmation.greetings}</p> <br/>
+                        <p className="md:text4xl sm:text-xl text-xl font-bold">-- {confirmation.name} --</p> <br/>
                     </div>
                 ))}
-            </div>
+            </Slider>
         </div>
     )
 }
